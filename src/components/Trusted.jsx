90f37c37b8c89ef42307dc04ce591ab61f2409e9
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-06-11 16:12:30 +0530

    trusted section added

--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -1,8 +1,31 @@
+import Description from "./common/Description"
+import Heading from "./common/Heading"
+import { trustedSecList } from "./common/Helper"
+import { Rating } from "./common/Icons"
 
 const Trusted = () => {
     return (
-        <div>
-            hello
+        <div className="relative py-10">
+            <div className=" container max-w-[1130px]">
+                <Heading className="max-w-[742px] mx-auto text-center" title="Trusted by Over 2,000 Designers, Developers, and Founders" />
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-[71px] max-h-[700px] overflow-scroll scroll-bar-hidden">
+                    {trustedSecList.map((item, index) => (
+                        <div key={index} className="w-full lg:max-w-[384px] h-min flex flex-col gap-[14px] bg-primary-lighter p-[30px] rounded-[20px]">
+                            <div className="flex items-center gap-[14px]">
+                                <img className="rounded-full" src={item.profilePhoto} alt={item.name} width={50} height={50} />
+                                <div className="flex flex-col gap-[2px]">
+                                    <p className="text-sm font-medium text-white sm:text-base">{item.name}</p>
+                                    <p className="text-xs font-light text-white sm:text-sm">{item.role}</p>
+                                </div>
+                            </div>
+                            <span><Rating /></span>
+                            <Description description={item.description} />
+                            <Description className="text-primary-light" description={item.date} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <span className="flex w-full h-[244px] bg-[linear-gradient(180deg,rgba(0,0,0,0)_0%,#000000_100%)] absolute bottom-0 left-0"></span>
         </div>
     )
 }
--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -3,6 +3,7 @@ import stuart from '../../assets/images/trusted/stuart.png'
 import dan from '../../assets/images/trusted/dan.png'
 import caitlyn from '../../assets/images/trusted/caitlyn.png'
 import devin from '../../assets/images/trusted/devin.png'
+import steve from '../../assets/images/trusted/steve.png'
 import nicolle from '../../assets/images/trusted/nicolle.png'
 import girish from '../../assets/images/trusted/girish.png'
 import vytas from '../../assets/images/trusted/vytas.png'
@@ -13,6 +14,7 @@ export const trustedSecList = [
     { profilePhoto: dan, name: "Dan Mall", role: "Design Systems Educator", description: "Tommy’s written 100 pages about how he designs, which I’ve read meticulously.", date: "Jul 5, 2023" },
     { profilePhoto: caitlyn, name: "Caitlyn Hutchison", role: "Design Lead", description: "One of the best design books I’ve read to date! Super practical. Excited for more to come 🧠", date: "Jul 23, 2023" },
     { profilePhoto: devin, name: "Devin Fountain", role: "Staff Product Designer", description: "Too many of you worry too much about the process and not enough about getting the thing done. This book will course correct your indecision.", date: "Jul 3, 2023" },
+    { profilePhoto: steve, name: "Steve Sewell", role: "CEO of Builder.io", description: "I’m loving this workbook! The step-by-step process and practical examples make it easy to make solid design decisions on the fly, even in our high-pressure, competitive market. And Tommy’s research and experience is evident. It’s like having a mentor in my back pocket.", date: "Jul 3, 2023" },
     { profilePhoto: nicolle, name: "Nicolle Hazard", role: "Product Designer & UX Researcher", description: "Hi Tommy, loved your book!", date: "May 10, 2023" },
     { profilePhoto: girish, name: "Girish Tewani", role: "Senior Product Designer", description: "Loving your book, looking forward to the updates, and thanks for answering my questions.", date: "Feb 13, 2023" },
     { profilePhoto: vytas, name: "Vytas Gaizutis", role: "Princial Product Designer", description: "Hi Tommy, I just purchased your book and starting to dig in. Really awesome so far! There is a ton of great value here.", date: "Feb 14, 2023" },
